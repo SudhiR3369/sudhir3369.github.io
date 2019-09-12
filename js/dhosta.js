0ae54f2b509b835ead991551fa7eb8b5ef290eaf
sudhir <sudhir.shrestha@braindigit.com>
2019-09-12 14:02:38 +0545

    css update

---
+++ js/dhosta.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";    
+    var now = new Date();
+    var once = new Date("09/29/2019 12:00:00 GMT+0545");
+    var time = once.getTime() / 1000 - now.getTime() / 1000;
+    $("#dhosta-clock").FlipClock(time, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+
+
+    /*
+    // Calling API
+    fetch("https://www.neptics.org.np/api/v1/user", {
+        method: 'GET',         
+        headers:{
+          'Content-Type': 'application/json'
+        }
+    }).then(resp => resp.json()
+    ).then(function(data){        
+        console.log(data);
+    });
+    */
+})(jQuery);
