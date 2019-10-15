6450e26901ee94ce3bbe24467a178ca411de87da
sudhir <sudhir.shrestha@braindigit.com>
2019-10-15 09:37:35 +0545

    dhosta portfolio adsense test

--- js/dhosta.js
+++ js/dhosta.js
@@ -3,10 +3,10 @@
     var now = new Date();
     var once = new Date("10/05/2019 00:01:00 GMT+0545");
     var time = once.getTime() / 1000 - now.getTime() / 1000;
-    $("#dhosta-clock").FlipClock(time, {
-        clockFace: 'DailyCounter',
-        countdown: true
-    });
+    // $("#dhosta-clock").FlipClock(time, {
+    //     clockFace: 'DailyCounter',
+    //     countdown: true
+    // });
     /*
     // Calling API
     fetch("https://www.neptics.org.np/api/v1/user", {
